b72e2a18415221f3b40493b048a6ad16056317c6
Sanket Garde <sanketgarde7473@gmail.com>
2023-12-14 11:51:25 +0530

    Added: Java functionality for Hotel Reservation to add the weekday and weekend rate rates for each hotel

--- src/main/java/com/bridgelabz/Hotel.java
+++ src/main/java/com/bridgelabz/Hotel.java
@@ -1,28 +1,51 @@
 package com.bridgelabz;
+
 /*
-*@Desc: Hotel class has two variables hotelName and a ratesForRegularCustomer.
-* Containing methods getter and setter getHotelName,getRatesForRegularCustomer,
-* setHotelName,setRatesForRegularCustomer.
+ *@Desc: Hotel class has two variables hotelName and a ratesForRegularCustomer.
+  Containing methods getter and setter getHotelName,getRatesForRegularCustomer,
+  setHotelName,setRatesForRegularCustomer,getWeekdaysRateForRegularCustomer,setWeekdaysRateForRegularCustomer,getWeekendRateForregularcustomer,
+  setWeekendRateForregularcustomer
+ * .
  */
 public class Hotel {
     String hotelName;
     int ratesForRegularCustomer;
+    int weekdaysRateForRegularCustomer;
+    int weekendRateForregularcustomer;
 
-    public Hotel(String hotelName, int ratesForRegularCustomer) {
+    public Hotel(String hotelName, int ratesForRegularCustomer, int weekdaysRateForRegularCustomer, int weekendRateForregularcustomer) {
         this.hotelName = hotelName;
         this.ratesForRegularCustomer = ratesForRegularCustomer;
+        this.weekdaysRateForRegularCustomer = weekdaysRateForRegularCustomer;
+        this.weekendRateForregularcustomer = weekendRateForregularcustomer;
+    }
+
+    public int getWeekdaysRateForRegularCustomer() {
+        return weekdaysRateForRegularCustomer;
+    }
+
+    public void setWeekdaysRateForRegularCustomer(int weekdaysRateForRegularCustomer) {
+        this.weekdaysRateForRegularCustomer = weekdaysRateForRegularCustomer;
+    }
+
+    public int getWeekendRateForregularcustomer() {
+        return weekendRateForregularcustomer;
+    }
+
+    public void setWeekendRateForregularcustomer(int weekendRateForregularcustomer) {
+        this.weekendRateForregularcustomer = weekendRateForregularcustomer;
     }
 
     public String getHotelName() {
         return hotelName;
     }
 
-    public int getRatesForRegularCustomer() {
-        return ratesForRegularCustomer;
-    }
-
     public void setHotelName(String hotelName) {
         this.hotelName = hotelName;
+    }
+
+    public int getRatesForRegularCustomer() {
+        return ratesForRegularCustomer;
     }
 
     public void setRatesForRegularCustomer(int ratesForRegularCustomer) {
